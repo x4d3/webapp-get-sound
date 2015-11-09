f669487de0298d5ee4900de463e8d20a59a4934d
x4d3 <moi@xade.eu>
2015-11-09 17:15:20 +0000

    adding get servlet

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -9,19 +10,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+
 @WebServlet(
         name = "MyServlet", 
         urlPatterns = {"/hello"}
     )
 public class HelloServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        ServletOutputStream out = resp.getOutputStream();
-        out.write("hello heroku".getBytes());
-        out.flush();
-        out.close();
-    }
-    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("audio/x-wav");
+		ServletOutputStream outStream = response.getOutputStream();
+		response.setHeader("Accept-Ranges", "bytes");
+		InputStream resourceAsStream = this.getClass().getResourceAsStream("sound.wav");
+		IOUtils.copy(resourceAsStream, outStream);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
+    
